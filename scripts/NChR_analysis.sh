ba7314f3cf3a2d6cf55371925f64365ce74cb6cd
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-06-23 15:12:43 -0500

    added files and command for HMMTOP

--- scripts/NChR_analysis.sh
+++ scripts/NChR_analysis.sh
@@ -110,78 +110,73 @@ pfam_HMM="$local_dir/auxillary/HMMs/Pfam-A.hmm"
 # done <"$species_gold"
 
 ###Parse hmm outputs for each species to get list of unique seq ids for R-A, R-P, G, F, A/S (can also use  if ($4 <= 1e-20) in awk)
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-		#Rhodopsin
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_1/' | sort -k4 -g > "${gold_out}"/${line}_Rhits.txt
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_1/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Rhits_ids.txt
-		curr_dir=$(dirname "${f}")
- 		gzcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${gold_out}"/${line}_Rhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_R.fa
-		#Glutamate
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_3/' | sort -k4 -g > "${gold_out}"/${line}_Ghits.txt
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_3/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Ghits_ids.txt
-		python "${seqextract_py}" "${gold_out}"/${line}_Ghits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_G.fa
-		#Adhesion/Secretin
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_2/' | sort -k4 -g > "${gold_out}"/${line}_AShits.txt
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_2/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_AShits_ids.txt
-		python "${seqextract_py}" "${gold_out}"/${line}_AShits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_AS.fa
-		#Frizzled
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/Frizzled/' | sort -k4 -g > "${gold_out}"/${line}_Fhits.txt
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/Frizzled/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Fhits_ids.txt
-		python "${seqextract_py}" "${gold_out}"/${line}_Fhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_F.fa
-		#Insect Odorant/ChemoR
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_6|7tm_7/' | sort -k4 -g > "${gold_out}"/${line}_IOChits.txt
-		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_6|7tm_7/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_IOChits_ids.txt
-		python "${seqextract_py}" "${gold_out}"/${line}_AShits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_IOC.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_gold"
-
-
-
-
-###HERE###
-
-
-
-		
-		# cd /Users/mzamanian/Bioinformatics/hmmtop_2.1/
-
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		#Rhodopsin
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_1/' | sort -k4 -g > "${gold_out}"/${line}_Rhits.txt
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_1/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Rhits_ids.txt
+# 		curr_dir=$(dirname "${f}")
+#  		gzcat "${f}" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${gold_out}"/${line}_Rhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_R.fa
+# 		#Glutamate
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_3/' | sort -k4 -g > "${gold_out}"/${line}_Ghits.txt
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_3/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Ghits_ids.txt
+# 		python "${seqextract_py}" "${gold_out}"/${line}_Ghits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_G.fa
+# 		#Adhesion/Secretin
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_2/' | sort -k4 -g > "${gold_out}"/${line}_AShits.txt
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_2/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_AShits_ids.txt
+# 		python "${seqextract_py}" "${gold_out}"/${line}_AShits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_AS.fa
+# 		#Frizzled
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/Frizzled/' | sort -k4 -g > "${gold_out}"/${line}_Fhits.txt
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/Frizzled/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_Fhits_ids.txt
+# 		python "${seqextract_py}" "${gold_out}"/${line}_Fhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_F.fa
+# 		#Insect Odorant/ChemoR
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_6|7tm_7/' | sort -k4 -g > "${gold_out}"/${line}_IOChits.txt
+# 		cat "${gold_out}"/${line}_hits.out | awk '{print $1 " " $3 " " $4 " " $5}' | awk '!/#/' | sort -k1,1 -k4,4g | sort -uk1,1 | awk '/7tm_6|7tm_7/' | sort -k4 -g  | awk '{if ($4 <= 1e-20) print $1}' > "${gold_out}"/${line}_IOChits_ids.txt
+# 		python "${seqextract_py}" "${gold_out}"/${line}_AShits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_IOC.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_gold"
 
 ######
 ###### PHYLOGENETIC ANALYSIS
 ######
 
-# phylo_dir="/Users/mzamanian/GitHub/ChemoR-sh/phylo/NemChR"
+phylo_dir="${local_dir}/phylo/NChR"
 
 # # # Copy sequence files to ../phylo/NemChR directory
-# # while IFS= read -r line; do
-# # 	cp ${genome_dir}/${line}/**/NChR/${line}_*.fa ${phylo_dir}
-# # done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cp "${gold_out}"/${line}_*.fa "${phylo_dir}"
+# 	done;
+# done <"$species_gold"
 
 # # # Cat and label files for alignment/phylo
-# # cat ${phylo_dir}/*_NCf.fa > ${phylo_dir}/All_NCf.fa
-# # cat ${phylo_dir}/c*_elegans_R.fa ${phylo_dir}/a*_suum_R.fa ${phylo_dir}/b*_malayi_R.fa ${phylo_dir}/s*_ratti_R.fa ${phylo_dir}/h*_contortus_R.fa ${phylo_dir}/t*_canis_R.fa > ${phylo_dir}/All_R.fa
-# # cat ${phylo_dir}/c*_elegans_F.fa ${phylo_dir}/a*_suum_F.fa ${phylo_dir}/b*_malayi_F.fa ${phylo_dir}/s*_ratti_F.fa ${phylo_dir}/h*_contortus_F.fa ${phylo_dir}/t*_canis_F.fa > ${phylo_dir}/All_F.fa
-# # cat ${phylo_dir}/c*_elegans_G.fa ${phylo_dir}/a*_suum_G.fa ${phylo_dir}/b*_malayi_G.fa ${phylo_dir}/s*_ratti_G.fa ${phylo_dir}/h*_contortus_G.fa ${phylo_dir}/t*_canis_G.fa > ${phylo_dir}/All_G.fa
-# # cat ${phylo_dir}/c*_elegans_AS.fa ${phylo_dir}/a*_suum_AS.fa ${phylo_dir}/b*_malayi_AS.fa ${phylo_dir}/s*_ratti_AS.fa ${phylo_dir}/h*_contortus_AS.fa ${phylo_dir}/t*_canis_AS.fa > ${phylo_dir}/All_AS.fa
+# cat "${phylo_dir}"/*_NCf.fa > "${phylo_dir}"/All_NCf.fa
+# cat "${phylo_dir}"/*_R.fa > "${phylo_dir}"/All_R.fa
+# cat "${phylo_dir}"/*_G.fa > "${phylo_dir}"/All_G.fa
+# cat "${phylo_dir}"/*_F.fa > "${phylo_dir}"/All_F.fa
+# cat "${phylo_dir}"/*_AS.fa > "${phylo_dir}"/All_AS.fa
+# cat "${phylo_dir}"/*_IOC.fa > "${phylo_dir}"/All_IOC.fa
 
 # # # Label each sequence with its predicted family
-# # sed 's/>/>NC:/' ${phylo_dir}/All_NCf.fa > ${phylo_dir}/All_NCf_lab.fa
-# # sed 's/>/>R:/' ${phylo_dir}/All_R.fa > ${phylo_dir}/All_Rf_lab.fa
-# # sed 's/>/>G:/' ${phylo_dir}/All_G.fa > ${phylo_dir}/All_Gf_lab.fa
-# # sed 's/>/>F:/' ${phylo_dir}/All_F.fa > ${phylo_dir}/All_Ff_lab.fa
-# # sed 's/>/>AS:/' ${phylo_dir}/All_AS.fa > ${phylo_dir}/All_ASf_lab.fa
-# # cat ${phylo_dir}/All*_lab.fa > ${phylo_dir}/All.fa
+# sed 's/>/>NC:/' "${phylo_dir}"/All_NCf.fa > "${phylo_dir}"/All_NCf_lab.fa
+# sed 's/>/>R:/' "${phylo_dir}"/All_R.fa > "${phylo_dir}"/All_Rf_lab.fa
+# sed 's/>/>G:/' "${phylo_dir}"/All_G.fa > "${phylo_dir}"/All_Gf_lab.fa
+# sed 's/>/>F:/' "${phylo_dir}"/All_F.fa > "${phylo_dir}"/All_Ff_lab.fa
+# sed 's/>/>AS:/' "${phylo_dir}"/All_AS.fa > "${phylo_dir}"/All_ASf_lab.fa
+# sed 's/>/>IOC:/' "${phylo_dir}"/All_IOC.fa > "${phylo_dir}"/All_IOCf_lab.fa
+# cat "${phylo_dir}"/All*_lab.fa > "${phylo_dir}"/All.fa
 
-# HMMextract_py="/Users/mzamanian/GitHub/ChemoR-sh/scripts/HMMTOP_extract.py"
+hmmtop="${gh_dir}"/scripts/auxillary/hmmtop2.1/./hmmtop
+cp "${gh_dir}"/scripts/auxillary/hmmtop_2.1/hmmtop.arch .
+cp "${gh_dir}"/scripts/auxillary/hmmtop_2.1/hmmtop.psv .
 
 # # # HMMTOP 
-# # cd /Users/mzamanian/Bioinformatics/hmmtop_2.1/
-# # ./hmmtop -if=${phylo_dir}/All.fa -of=${phylo_dir}/hmm_output.txt -sf=FAS
-# # Parse HHMTOP output to get list of seq ids with >= 5 TM domains <= 10 TM domains
-# #python ${HMMextract_py} ${phylo_dir}/hmm_output.txt ${phylo_dir}/All.fa ${phylo_dir}/All_TMfiltered.fa
+#cd /Users/mzamanian/Bioinformatics/hmmtop_2.1/
+hmmtop -if="${phylo_dir}"/All.fa -of="${phylo_dir}"/hmm_output.txt -sf=FAS
+#Parse HHMTOP output to get list of seq ids with >= 5 TM domains <= 10 TM domains
+python "${HMMextract_py}" "${phylo_dir}"/hmm_output.txt "${phylo_dir}"/All.fa "${phylo_dir}"/All_TMfiltered.fa
 
 # # #Align files
 # #mafft --op 2 --ep 1 --thread 2 --maxiterate 1 ${phylo_dir}/All_TMfiltered.fa > ${phylo_dir}/All_TMfiltered_align.fa
