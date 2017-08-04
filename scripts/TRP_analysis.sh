a156943ce110e811f87d0ad72eccf5a04f432b81
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-08-04 09:31:30 -0500

    Trimming

--- scripts/TRP_analysis.sh
+++ scripts/TRP_analysis.sh
@@ -62,184 +62,184 @@ mkdir "$local_dir/auxillary/HMMs"
 pfam_HMM="${local_dir}"/auxillary/HMMs/Pfam-A.hmm
 
 ## TRPs
-seeds="${gh_dir}"/auxillary/TRP/trp_seeds.fa
+# seeds="${gh_dir}"/auxillary/TRP/trp_seeds.fa
 
-mafft --thread 4 --auto "${seeds}" > "${gh_dir}"/auxillary/TRP/trp_seeds.aln
-trimal -gt 0.7 -cons 2 -in "${gh_dir}"/auxillary/TRP/trp_seeds.aln -out "${gh_dir}"/auxillary/TRP/trp_seeds_trim.aln
-hmmbuild "${gh_dir}"/auxillary/TRP/TRP.hmm "${gh_dir}"/auxillary/TRP/trp_seeds_trim.aln
-TRP_HMM="${gh_dir}"/auxillary/TRP/TRP.hmm
+# mafft --thread 4 --auto "${seeds}" > "${gh_dir}"/auxillary/TRP/trp_seeds.aln
+# trimal -gt 0.7 -cons 2 -in "${gh_dir}"/auxillary/TRP/trp_seeds.aln -out "${gh_dir}"/auxillary/TRP/trp_seeds_trim.aln
+# hmmbuild "${gh_dir}"/auxillary/TRP/TRP.hmm "${gh_dir}"/auxillary/TRP/trp_seeds_trim.aln
+# TRP_HMM="${gh_dir}"/auxillary/TRP/TRP.hmm
 
-### GOLD GENOMES - Prepare BLAST DBs
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-	  	#echo "${curr_dir}"
-		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
-		cd "${curr_dir}"
-		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
-	done;
-done <"$species_gold"
+# ### GOLD GENOMES - Prepare BLAST DBs
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 	  	#echo "${curr_dir}"
+# 		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
+# 		cd "${curr_dir}"
+# 		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - Prepare BLAST DBs
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-	  	#echo "${curr_dir}"
-		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
-		cd "${curr_dir}"
-		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 	  	#echo "${curr_dir}"
+# 		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
+# 		cd "${curr_dir}"
+# 		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
+# 	done;
+# done <"$species_ngf"
 
 ### GOLD GENOMES - mine for TRPs, start with reciprocal blast
 # line = species name, iterate through gold genome species names
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		#blast TRP seeds against all proteomes, using E-value cutoff
-		cd "${curr_dir}"
-		blastp -query "${seeds}" -db ${line}.protein.db -out "${gold_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		#blast TRP seeds against all proteomes, using E-value cutoff
+# 		cd "${curr_dir}"
+# 		blastp -query "${seeds}" -db ${line}.protein.db -out "${gold_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - mine for TRPs, start with reciprocal blast
 # line = species name, iterate through gold genome species names
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		#blast TRP seeds against all proteomes, using E-value cutoff
-		cd "${curr_dir}"
-		blastp -query "${seeds}" -db ${line}.protein.db -out "${ngf_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		#blast TRP seeds against all proteomes, using E-value cutoff
+# 		cd "${curr_dir}"
+# 		blastp -query "${seeds}" -db ${line}.protein.db -out "${ngf_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_ngf"
 
 ### GOLD GENOMES - extract sequences of surviving hits
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-		cat "${gold_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${gold_out}"/${line}_blast_TRPhits.txt
-		cat "${gold_out}"/${line}_blast_TRPhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${gold_out}"/${line}_blast_TRPhits_ids.txt
-		#Extract these sequences
-		curr_dir=$(dirname "${f}")
-		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${gold_out}"/${line}_blast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_blast_TRP.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cat "${gold_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${gold_out}"/${line}_blast_TRPhits.txt
+# 		cat "${gold_out}"/${line}_blast_TRPhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${gold_out}"/${line}_blast_TRPhits_ids.txt
+# 		#Extract these sequences
+# 		curr_dir=$(dirname "${f}")
+# 		#echo ${curr_dir}
+# 		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${gold_out}"/${line}_blast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_blast_TRP.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - extract sequences of surviving hits
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-		cat "${ngf_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${ngf_out}"/${line}_blast_TRPhits.txt
-		cat "${ngf_out}"/${line}_blast_TRPhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${ngf_out}"/${line}_blast_TRPhits_ids.txt
-		#Extract these sequences
-		curr_dir=$(dirname "${f}")
-		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${ngf_out}"/${line}_blast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_blast_TRP.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cat "${ngf_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${ngf_out}"/${line}_blast_TRPhits.txt
+# 		cat "${ngf_out}"/${line}_blast_TRPhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${ngf_out}"/${line}_blast_TRPhits_ids.txt
+# 		#Extract these sequences
+# 		curr_dir=$(dirname "${f}")
+# 		#echo ${curr_dir}
+# 		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${ngf_out}"/${line}_blast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_blast_TRP.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_ngf"
 
 ### GOLD GENOMES - Reciprocal BLAST of extracted sequences against C. elegans proteome
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		#blast TRP seeds against all proteomes, using E-value cutoff
-		cd "${gold_dir}"/caenorhabditis_elegans/PRJNA13758/
-		blastp -query "${gold_out}"/${line}_blast_TRP.fa -db caenorhabditis_elegans.protein.db -out "${gold_out}"/${line}_rec.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		#blast TRP seeds against all proteomes, using E-value cutoff
+# 		cd "${gold_dir}"/caenorhabditis_elegans/PRJNA13758/
+# 		blastp -query "${gold_out}"/${line}_blast_TRP.fa -db caenorhabditis_elegans.protein.db -out "${gold_out}"/${line}_rec.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - Reciprocal BLAST of extracted sequences against C. elegans proteome
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		#blast TRP seeds against all proteomes, using E-value cutoff
-		cd "${gold_dir}"/caenorhabditis_elegans/PRJNA13758/
-		blastp -query "${ngf_out}"/${line}_blast_TRP.fa -db caenorhabditis_elegans.protein.db -out "${ngf_out}"/${line}_rec.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		#blast TRP seeds against all proteomes, using E-value cutoff
+# 		cd "${gold_dir}"/caenorhabditis_elegans/PRJNA13758/
+# 		blastp -query "${ngf_out}"/${line}_blast_TRP.fa -db caenorhabditis_elegans.protein.db -out "${ngf_out}"/${line}_rec.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_ngf"
 
 ### GOLD GENOMES - remove hits that aren't most similar to a TRP, extract sequences of surviving hits
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-		cat "${gold_out}"/${line}_rec.blastout | awk '{print $1 " " $2 " " $7}' | sort -k1,1 -k3,3g | sort -uk1,1 | awk '/C29E6.2.|M05B5.6.|Y73F8A.1.|ZK512.3.|T01H8.5.|F54D1.5.|C05C12.3.|ZC21.2.|R06B10.4.|K01A11.4.|R13A5.1.|B0212.5.|F28H7.10.|T09A12.3.|T10B10.7.|Y40C5A.2.|Y71A12B.4./' | sort -k3 -g > "${gold_out}"/${line}_rblast_TRPhits.txt
-		cat "${gold_out}"/${line}_rblast_TRPhits.txt | awk '{print $1}' > "${gold_out}"/${line}_rblast_TRPhits_ids.txt
-		#Extract these sequences
-		curr_dir=$(dirname "${f}")
-		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${gold_out}"/${line}_rblast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_rblast_TRP.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cat "${gold_out}"/${line}_rec.blastout | awk '{print $1 " " $2 " " $7}' | sort -k1,1 -k3,3g | sort -uk1,1 | awk '/C29E6.2.|M05B5.6.|Y73F8A.1.|ZK512.3.|T01H8.5.|F54D1.5.|C05C12.3.|ZC21.2.|R06B10.4.|K01A11.4.|R13A5.1.|B0212.5.|F28H7.10.|T09A12.3.|T10B10.7.|Y40C5A.2.|Y71A12B.4./' | sort -k3 -g > "${gold_out}"/${line}_rblast_TRPhits.txt
+# 		cat "${gold_out}"/${line}_rblast_TRPhits.txt | awk '{print $1}' > "${gold_out}"/${line}_rblast_TRPhits_ids.txt
+# 		#Extract these sequences
+# 		curr_dir=$(dirname "${f}")
+# 		#echo ${curr_dir}
+# 		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${gold_out}"/${line}_rblast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_rblast_TRP.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - remove hits that aren't most similar to a TRP, extract sequences of surviving hits
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-		cat "${ngf_out}"/${line}_rec.blastout | awk '{print $1 " " $2 " " $7}' | sort -k1,1 -k3,3g | sort -uk1,1 | awk '/C29E6.2.|M05B5.6.|Y73F8A.1.|ZK512.3.|T01H8.5.|F54D1.5.|C05C12.3.|ZC21.2.|R06B10.4.|K01A11.4.|R13A5.1.|B0212.5.|F28H7.10.|T09A12.3.|T10B10.7.|Y40C5A.2.|Y71A12B.4./' | sort -k3 -g > "${ngf_out}"/${line}_rblast_TRPhits.txt
-		cat "${ngf_out}"/${line}_rblast_TRPhits.txt | awk '{print $1}' > "${ngf_out}"/${line}_rblast_TRPhits_ids.txt
-		#Extract these sequences
-		curr_dir=$(dirname "${f}")
-		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${ngf_out}"/${line}_rblast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_rblast_TRP.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cat "${ngf_out}"/${line}_rec.blastout | awk '{print $1 " " $2 " " $7}' | sort -k1,1 -k3,3g | sort -uk1,1 | awk '/C29E6.2.|M05B5.6.|Y73F8A.1.|ZK512.3.|T01H8.5.|F54D1.5.|C05C12.3.|ZC21.2.|R06B10.4.|K01A11.4.|R13A5.1.|B0212.5.|F28H7.10.|T09A12.3.|T10B10.7.|Y40C5A.2.|Y71A12B.4./' | sort -k3 -g > "${ngf_out}"/${line}_rblast_TRPhits.txt
+# 		cat "${ngf_out}"/${line}_rblast_TRPhits.txt | awk '{print $1}' > "${ngf_out}"/${line}_rblast_TRPhits_ids.txt
+# 		#Extract these sequences
+# 		curr_dir=$(dirname "${f}")
+# 		#echo ${curr_dir}
+# 		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${ngf_out}"/${line}_rblast_TRPhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_rblast_TRP.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_ngf"
 
 ######
 ###### PHYLOGENETIC ANALYSIS
 ######
 
 ### GOLD GENOMES - Copy sequence files to ../phylo/ directory
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-		cp "${gold_out}"/${line}_rblast_TRP.fa "${phylo_out}"
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cp "${gold_out}"/${line}_rblast_TRP.fa "${phylo_out}"
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - Copy sequence files to ../phylo/ directory
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-		cp "${ngf_out}"/${line}_rblast_TRP.fa "${phylo_out}"
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cp "${ngf_out}"/${line}_rblast_TRP.fa "${phylo_out}"
+# 	done;
+# done <"$species_ngf"
 
 ### Label each sequence with its species name
-for f in "${phylo_out}"/*_rblast_TRP.fa ; do
-	python "${change_ID_py}" "$f" "$f".fa;
-done
-for f in "${phylo_out}"/*.fa.fa ; do
-	mv "$f" "${f/.fa.fa/_label.fa}";
-done
+# for f in "${phylo_out}"/*_rblast_TRP.fa ; do
+# 	python "${change_ID_py}" "$f" "$f".fa;
+# done
+# for f in "${phylo_out}"/*.fa.fa ; do
+# 	mv "$f" "${f/.fa.fa/_label.fa}";
+# done
 
 # ### Remove Pristionchus and Panagrellus
-for f in "${phylo_out}"/panagrellus*; do mv "$f" "${f/.fa/.fa.bkp}"; done
-for f in "${phylo_out}"/pristionchus*; do mv "$f" "${f/.fa/.fa.bkp}"; done
+# for f in "${phylo_out}"/panagrellus*; do mv "$f" "${f/.fa/.fa.bkp}"; done
+# for f in "${phylo_out}"/pristionchus*; do mv "$f" "${f/.fa/.fa.bkp}"; done
 
 ### Cat and label files for alignment/phylo
-cat "${phylo_out}"/*_rblast_TRP_label.fa > "${phylo_out}"/All_rblast_TRP_label.fa
+# cat "${phylo_out}"/*_rblast_TRP_label.fa > "${phylo_out}"/All_rblast_TRP_label.fa
 
 ### Pull in manually curated outgroup (seeds)
-cat "${phylo_out}"/All_rblast_TRP_label.fa "${seeds}" > "${phylo_out}"/All_rblast_TRP_outgroup.fa
+# cat "${phylo_out}"/All_rblast_TRP_label.fa "${seeds}" > "${phylo_out}"/All_rblast_TRP_outgroup.fa
 
 
 ### HMMTOP
-cd "${gh_dir}"/scripts/auxillary/hmmtop_2.1/
-./hmmtop -if="${phylo_out}"/All_rblast_TRP_outgroup.fa -of="${phylo_out}"/All_rblast_TRP_outgroup_hmmtop_output.txt -sf=FAS
+# cd "${gh_dir}"/scripts/auxillary/hmmtop_2.1/
+# ./hmmtop -if="${phylo_out}"/All_rblast_TRP_outgroup.fa -of="${phylo_out}"/All_rblast_TRP_outgroup_hmmtop_output.txt -sf=FAS
 ## Remove sequences without any predicted TMs
-cat "${phylo_out}"/All_rblast_TRP_outgroup_hmmtop_output.txt | awk '{print $3 " " $5}' | awk '$2!=0' | awk '{print $1}' > "${phylo_out}"/All_rblast_TRP_outgroup.key
-python "${seqextract_py}" "${phylo_out}"/All_rblast_TRP_outgroup.key "${phylo_out}"/All_rblast_TRP_outgroup.fa "${phylo_out}"/All_rblast_TRPf_outgroup.fa
+# cat "${phylo_out}"/All_rblast_TRP_outgroup_hmmtop_output.txt | awk '{print $3 " " $5}' | awk '$2!=0' | awk '{print $1}' > "${phylo_out}"/All_rblast_TRP_outgroup.key
+# python "${seqextract_py}" "${phylo_out}"/All_rblast_TRP_outgroup.key "${phylo_out}"/All_rblast_TRP_outgroup.fa "${phylo_out}"/All_rblast_TRPf_outgroup.fa
 
 
 ### Align files
-einsi --thread 4 "${phylo_out}"/All_rblast_TRPf_outgroup.fa > "${phylo_out}"/All_rblast_TRPf_outgroup.aln
+# einsi --thread 4 "${phylo_out}"/All_rblast_TRPf_outgroup.fa > "${phylo_out}"/All_rblast_TRPf_outgroup.aln
 
 ### Trim alignments
-# trimal_cmd="${gh_dir}"/scripts/auxillary/trimal/source/./trimal
-# "${trimal_cmd}" -in "${phylo_out}"/All_rblast_TRPf_outgroup.aln -out "${phylo_out}"/All_rblast_TRPf_outgroup_trim.aln -gt 0.75 -cons 2
+trimal_cmd="${gh_dir}"/scripts/auxillary/trimal/source/./trimal
+"${trimal_cmd}" -in "${phylo_out}"/All_rblast_TRPf_outgroup.aln -out "${phylo_out}"/All_rblast_TRPf_outgroup_trim.aln -gt 0.75 -cons 2
 
 # on server
 # ssh zamanian@brc6.secure.biotech.wisc.edu 'nohup ~/install/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -f a -x 12345 -p 12345 -# 100 -m PROTCATAUTO -s ~/data/TRP/All_rblast_TRPf_outgroup_trim.aln -n TRP &'
