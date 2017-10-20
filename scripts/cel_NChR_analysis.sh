9af1263dc4047193b9041e034f05c95e3deeb790
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2017-10-20 10:02:01 -0500

    Add raxml command

--- scripts/cel_NChR_analysis.sh
+++ scripts/cel_NChR_analysis.sh
@@ -37,13 +37,16 @@ cel_out="${local_dir}/NChR/phylo/c_elegans/"
 # einsi --thread 8 "${cel_out}"/caenorhabditis_elegans_NCf_label.fa > "${cel_out}"/caenorhabditis_elegans_NCf_label.aln
 ## Email when complete
 # mailx -s "Alignment complete!" njwheeler@wisc.edu <<< "The alignment of caenorhabditis_elegans_NCf_label has successfully completed."
-## Trim
+## Trim and filter (filters out 120 sequences with < 70% overlap with the rest of the alignment)
 trimal_cmd="${gh_dir}"/scripts/auxillary/trimal/source/./trimal
-"${trimal_cmd}" -in "${cel_out}"/caenorhabditis_elegans_NCf_label.aln -out "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln -gt 0.7 
-"${trimal_cmd}" -in "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln -out "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter.aln -resoverlap 0.70 -seqoverlap 70
+# "${trimal_cmd}" -in "${cel_out}"/caenorhabditis_elegans_NCf_label.aln -out "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln -gt 0.7 
+# "${trimal_cmd}" -in "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln -out "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter.aln -resoverlap 0.70 -seqoverlap 70
 ## Make single-line FASTA and get IDs to compare lists
-awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln > "${cel_out}"/caenorhabditis_elegans_NCf_trim-single.aln
-awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter.aln > "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter-single.aln
-awk 'NR%2==1' "${cel_out}"/caenorhabditis_elegans_NCf_trim-single.aln > "${cel_out}"/trimmed_ids.txt
-awk 'NR%2==1' $"${cel_out}"/caenorhabditis_elegans_NCf_trim_filter-single.aln > "${cel_out}"/filtered_ids.txt
-grep -v -f "${cel_out}"/filtered_ids.txt "${cel_out}"/trimmed_ids.txt > "${cel_out}"/missing_ids.txt
+# awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < "${cel_out}"/caenorhabditis_elegans_NCf_trim.aln > "${cel_out}"/caenorhabditis_elegans_NCf_trim-single.aln
+# awk '/^>/ {printf("%s%s\n",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' < "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter.aln > "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter-single.aln
+# awk 'NR%2==1' "${cel_out}"/caenorhabditis_elegans_NCf_trim-single.aln > "${cel_out}"/trimmed_ids.txt
+# awk 'NR%2==1' $"${cel_out}"/caenorhabditis_elegans_NCf_trim_filter-single.aln > "${cel_out}"/filtered_ids.txt
+# grep -v -f "${cel_out}"/filtered_ids.txt "${cel_out}"/trimmed_ids.txt > "${cel_out}"/missing_ids.txt
+
+### ML tree inference
+/home/BIOTECH/zamanian/install/standard-RAxML/raxmlHPC-PTHREADS-SSE3 -T 4 -f a -x 12345 -p 12345 -# 100 -m PROTGAMMAAUTO -s "${cel_out}"/caenorhabditis_elegans_NCf_trim_filter.aln -n "${cel_out}"/caenorhabditis_elegans_ML
