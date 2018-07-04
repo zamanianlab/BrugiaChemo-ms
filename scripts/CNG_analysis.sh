a6152fab51ee0b0eecce15f42dcd9f637473998c
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2018-07-04 09:55:59 -0500

    Completed CNG identification

--- scripts/CNG_analysis.sh
+++ scripts/CNG_analysis.sh
@@ -16,14 +16,14 @@ ngf_dir="${local_dir}/ngf_genomes"
 ngo_dir="${local_dir}/ngo_genomes"
 
 ### Define script output directories
-mkdir "${local_dir}/CNG/"
-mkdir "${local_dir}/CNG/g_genomes"
+# mkdir "${local_dir}/CNG/"
+# mkdir "${local_dir}/CNG/g_genomes"
 gold_out="${local_dir}/CNG/g_genomes"
-mkdir "${local_dir}/CNG/ngf_genomes"
+# mkdir "${local_dir}/CNG/ngf_genomes"
 ngf_out="${local_dir}/CNG/ngf_genomes"
-mkdir "${local_dir}/CNG/ngo_genomes"
+# mkdir "${local_dir}/CNG/ngo_genomes"
 ngo_out="${local_dir}/CNG/ngo_genomes"
-mkdir "${local_dir}/CNG/phylo"
+# mkdir "${local_dir}/CNG/phylo"
 phylo_out="${local_dir}/CNG/phylo"
 # mkdir "${local_dir}/CNG/mcl"
 # mcl_out="${local_dir}/CNG/mcl"
@@ -37,10 +37,10 @@ HMMTOP_py="${gh_dir}"/scripts/auxillary/HMMTOP_extract.py
 change_ID_py="${gh_dir}"/scripts/auxillary/id_change.py
 
 ## Prepare Pfam-A HMM db
-mkdir "$local_dir/auxillary"
-mkdir "$local_dir/auxillary/HMMs"
+# mkdir "$local_dir/auxillary"
+# mkdir "$local_dir/auxillary/HMMs"
 # wget -nc -O "$local_dir/auxillary/HMMs/Pfam-A.hmm.gz" ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
-# zcat "$local_dir/auxillary/HMMs/Pfam-A.hmm.gz" > "$local_dir/auxillary/HMMs/Pfam-A.hmm"
+# gunzip -c "$local_dir/auxillary/HMMs/Pfam-A.hmm.gz" > "$local_dir/auxillary/HMMs/Pfam-A.hmm"
 # hmmpress "$local_dir/auxillary/HMMs/Pfam-A.hmm"
 pfam_HMM="${local_dir}"/auxillary/HMMs/Pfam-A.hmm
 
@@ -48,9 +48,9 @@ pfam_HMM="${local_dir}"/auxillary/HMMs/Pfam-A.hmm
 ## CNGs
 seeds="${gh_dir}"/auxillary/CNG/cng_seeds.fa
 
-mafft --thread 4 --auto "${seeds}" > "${gh_dir}"/auxillary/CNG/cng_seeds.aln
-trimal -gt 0.7 -cons 2 -in "${gh_dir}"/auxillary/CNG/cng_seeds.aln -out "${gh_dir}"/auxillary/CNG/cng_seeds_trim.aln
-hmmbuild "${gh_dir}"/auxillary/CNG/cng.hmm "${gh_dir}"/auxillary/CNG/cng_seeds_trim.aln
+# mafft --thread 4 --auto "${seeds}" > "${gh_dir}"/auxillary/CNG/cng_seeds.aln
+# trimal -gt 0.7 -cons 2 -in "${gh_dir}"/auxillary/CNG/cng_seeds.aln -out "${gh_dir}"/auxillary/CNG/cng_seeds_trim.aln
+# hmmbuild "${gh_dir}"/auxillary/CNG/cng.hmm "${gh_dir}"/auxillary/CNG/cng_seeds_trim.aln
 CNG_HMM="${gh_dir}"/auxillary/CNG/cng.hmm
 
 # ### GOLD GENOMES - Prepare BLAST DBs
@@ -58,7 +58,7 @@ CNG_HMM="${gh_dir}"/auxillary/CNG/cng.hmm
 # 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
 # 	  	curr_dir=$(dirname "${f}")
 # 	  	#echo "${curr_dir}"
-# 		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
+# 		gunzip -c "${f}" > "${curr_dir}"/${line}.protein.fa
 # 		cd "${curr_dir}"
 # 		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
 # 	done;
@@ -69,7 +69,7 @@ CNG_HMM="${gh_dir}"/auxillary/CNG/cng.hmm
 # 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
 # 	  	curr_dir=$(dirname "${f}")
 # 	  	#echo "${curr_dir}"
-# 		zcat "${f}" > "${curr_dir}"/${line}.protein.fa
+# 		gunzip -c "${f}" > "${curr_dir}"/${line}.protein.fa
 # 		cd "${curr_dir}"
 # 		makeblastdb -dbtype prot -in ${line}.protein.fa -out ${line}.protein.db
 # 	done;
@@ -77,39 +77,39 @@ CNG_HMM="${gh_dir}"/auxillary/CNG/cng.hmm
 
 ### GOLD GENOMES - mine for CNGs, start with reciprocal blast
 # line = species name, iterate through gold genome species names
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		# blast CNG seeds against all proteomes, using E-value cutoff
-		cd "${curr_dir}"
-		blastp -query "${seeds}" -db ${line}.protein.db -out "${gold_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		# blast CNG seeds against all proteomes, using E-value cutoff
+# 		cd "${curr_dir}"
+# 		blastp -query "${seeds}" -db ${line}.protein.db -out "${gold_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - mine for CNGs, start with reciprocal blast
 # line = species name, iterate through gold genome species names
-while IFS= read -r line; do
-	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
-	  	curr_dir=$(dirname "${f}")
-		# blast CNG seeds against all proteomes, using E-value cutoff
-		cd "${curr_dir}"
-		blastp -query "${seeds}" -db ${line}.protein.db -out "${ngf_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
-	done;
-done <"$species_ngf"
+# while IFS= read -r line; do
+# 	for f in "${ngf_dir}"/${line}/**/*.protein.fa.gz ; do
+# 	  	curr_dir=$(dirname "${f}")
+# 		# blast CNG seeds against all proteomes, using E-value cutoff
+# 		cd "${curr_dir}"
+# 		blastp -query "${seeds}" -db ${line}.protein.db -out "${ngf_out}"/${line}.blastout -num_threads 4 -evalue 0.01 -outfmt '6 qseqid sseqid pident ppos length mismatch evalue bitscore'
+# 	done;
+# done <"$species_ngf"
 
 ### GOLD GENOMES - extract sequences of surviving hits
-while IFS= read -r line; do
-	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
-		cat "${gold_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${gold_out}"/${line}_blast_CNGhits.txt
-		cat "${gold_out}"/${line}_blast_CNGhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${gold_out}"/${line}_blast_CNGhits_ids.txt
-		#Extract these sequences
-		curr_dir=$(dirname "${f}")
-		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
-		python "${seqextract_py}" "${gold_out}"/${line}_blast_CNGhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_blast_CNG.fa
-		rm "${curr_dir}"/protein.tmp.fa
-	done;
-done <"$species_gold"
+# while IFS= read -r line; do
+# 	for f in "${gold_dir}"/${line}/**/*.protein.fa.gz ; do
+# 		cat "${gold_out}"/${line}.blastout | awk '{print $1 " " $2 " " $7}' > "${gold_out}"/${line}_blast_CNGhits.txt
+# 		cat "${gold_out}"/${line}_blast_CNGhits.txt | awk '{print $2}' | awk '!seen[$0]++' > "${gold_out}"/${line}_blast_CNGhits_ids.txt
+# 		#Extract these sequences
+# 		curr_dir=$(dirname "${f}")
+# 		#echo ${curr_dir}
+# 		gunzip -c "$f" > "${curr_dir}"/protein.tmp.fa
+# 		python "${seqextract_py}" "${gold_out}"/${line}_blast_CNGhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_blast_CNG.fa
+# 		rm "${curr_dir}"/protein.tmp.fa
+# 	done;
+# done <"$species_gold"
 
 ### NON-GOLD FILARID GENOMES - extract sequences of surviving hits
 while IFS= read -r line; do
@@ -119,7 +119,7 @@ while IFS= read -r line; do
 		#Extract these sequences
 		curr_dir=$(dirname "${f}")
 		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+		gunzip -c "${f}" > "${curr_dir}"/protein.tmp.fa
 		python "${seqextract_py}" "${ngf_out}"/${line}_blast_CNGhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_blast_CNG.fa
 		rm "${curr_dir}"/protein.tmp.fa
 	done;
@@ -153,7 +153,7 @@ while IFS= read -r line; do
 		#Extract these sequences
 		curr_dir=$(dirname "${f}")
 		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+		gunzip -c "${f}" > "${curr_dir}"/protein.tmp.fa
 		python "${seqextract_py}" "${gold_out}"/${line}_rblast_CNGhits_ids.txt "${curr_dir}"/protein.tmp.fa "${gold_out}"/${line}_rblast_CNG.fa
 		rm "${curr_dir}"/protein.tmp.fa
 	done;
@@ -167,7 +167,7 @@ while IFS= read -r line; do
 		#Extract these sequences
 		curr_dir=$(dirname "${f}")
 		#echo ${curr_dir}
-		zcat "${f}" > "${curr_dir}"/protein.tmp.fa
+		gunzip -c "${f}" > "${curr_dir}"/protein.tmp.fa
 		python "${seqextract_py}" "${ngf_out}"/${line}_rblast_CNGhits_ids.txt "${curr_dir}"/protein.tmp.fa "${ngf_out}"/${line}_rblast_CNG.fa
 		rm "${curr_dir}"/protein.tmp.fa
 	done;
--- scripts/auxillary/seq_extract.py
+++ scripts/auxillary/seq_extract.py
@@ -1,32 +1,34 @@
-#!~/install/anaconda3/bin/python
+#!/Users/njwheeler/software/miniconda3/bin/python
 
-import collections
-from collections import Counter
-from sys import argv 
-script, filename1, filename2, filename3 = argv 
-import re
+# import collections
+# from collections import Counter
+from sys import argv
+# import re
 from Bio import SeqIO
 
+script, filename1, filename2, filename3 = argv
 
-p = open(filename1) #list of GeneIDs in list/txt format
-record_dict = SeqIO.index(filename2, "fasta") #proteome fasta file
-output_handle = open(filename3, "w") #output file of extracted sequences
+p = open(filename1)  # list of GeneIDs in list/txt format
+record_dict = SeqIO.index(filename2, "fasta")  # proteome fasta file
+output_handle = open(filename3, "w")  # output file of extracted sequences
+
 
 def seqextract(file):
-	for line in file:
-		ids = []
-		line = line.strip()
-		ids.append(line)
-		#print ids
-		for entry in ids:
-			# print entry
-			try:
-				current = record_dict[entry]
-				SeqIO.write(current, output_handle, "fasta")
-			except KeyError:
-				print("Could not find {} in {}.".format(entry, filename2))
-		else: 
-			pass
+    for line in file:
+        ids = []
+        line = line.strip()
+        ids.append(line)
+        # print ids
+        for entry in ids:
+            # print entry
+            try:
+                current = record_dict[entry]
+                SeqIO.write(current, output_handle, "fasta")
+            except KeyError:
+                print("Could not find {} in {}.".format(entry, filename2))
+            else:
+                pass
+
 
 run = seqextract(p)
 output_handle.close()
