5f4ba5b9cccb193968863e00b3c0e4923a4a101a
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2018-10-15 07:34:20 -0500

    Begin preparing for publication

--- scripts/CHEMO_HEATMAP.R
+++ scripts/CHEMO_HEATMAP.R
@@ -1,7 +1,6 @@
 library(tidyverse)
-library(viridis)
 
-setwd("~/Box Sync/GHdata/50HGI/NChR/phylo/ML/")
+setwd("~/Box Sync/GHdata/50HGI/ChemoR/phylo/ML/")
 
 #########################################################################################################
 ######################                                                             ######################
@@ -12,7 +11,7 @@ setwd("~/Box Sync/GHdata/50HGI/NChR/phylo/ML/")
 tree <- read_csv("family_clades.csv")
 
 # load in reference file matching species <-> clade
-reference <- read.csv("~/Box Sync/ZamanianLab/Data/Phylogenetics/ChemoR/clade_species_phylum.csv", header = FALSE) %>%
+reference <- read.csv("~/Box Sync/ZamanianLab/Data/Genomics/Phylogenetics/ChemoR/tree/clade_species_phylum.csv", header = FALSE) %>%
   rename(Species = V1, Clade = V2, Phylum = V3)
 species_info <- read.csv("~/Box Sync/GitHub/50HGI/auxillary/species_info.csv", header = TRUE) %>%
   select(-BioProject, -Classification)
@@ -70,9 +69,12 @@ plot <- ggplot(df.p, aes(Full, Family)) +
         panel.background = element_blank(),
         axis.text.x = element_text(size = 10, angle = 60, hjust = 1),
         strip.text = element_text(face = "bold")) +
-  scale_fill_viridis(name = "Normalized Gene Count", option = "plasma")
+  scale_fill_viridis_c(name = "Normalized Gene Count", option = "plasma") +
+  NULL
 # plot
 
+
+# change facet labels
 gtab <- ggplot_gtable(ggplot_build(plot))
 stript <- which(grepl('strip-t', gtab$layout$name))
 fillt <- c("#a361c7", "#58a865", "#c65c8a", "gray", "#9b9c3b", "#648ace", "#c98443", "palegreen", "#cb4f42")
